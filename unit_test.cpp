2063bec39c69342bf8e3f998ebf52ae68d517865
Julian van Doorn <gigajulian@gmail.com>
2018-04-24 19:03:16 +0200

    Fixed memory leak

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
 #include <string>
 
-void memoryLeakFunction() {
-	std::string* str = new std::string("stuff");
-}
-
 int main() {
 	std::cout << "Test successful!" << std::endl;
 	std::cout << "Second line!" << std::endl;
-
-	memoryLeakFunction();
-
 	return 0;
 }
