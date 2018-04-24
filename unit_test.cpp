e7299b4506798c6fba16ba98faffdb2db9cf7c08
Julian van Doorn <gigajulian@gmail.com>
2018-04-24 18:59:28 +0200

    Added memory leaking function

--- unit_test.cpp
+++ unit_test.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <string>
+
+void memoryLeakFunction() {
+	std::string* str = new std::string("stuff");
+}
 
 int main() {
 	std::cout << "Test successful!" << std::endl;
 	std::cout << "Second line!" << std::endl;
+
+	memoryLeakFunction();
+
 	return 0;
 }
