1e1595acf64e719c1af9eedff59225540fcb207d
Julian van Doorn <gigajulian@gmail.com>
2018-05-04 12:37:01 +0200

    Fixed CI for single module

--- modules/SHORTY/BuildModule.cmake
+++ modules/SHORTY/BuildModule.cmake
@@ -1,6 +1,6 @@
-include(../../flags.cmake)
+include (../../flags.cmake)
 
-set(cxxflags
+set (cxxflags
     "-Os"
     "-ffunction-sections"
     "-fdata-sections"
@@ -8,14 +8,14 @@ set(cxxflags
     "-fno-rtti"
     "-fno-threadsafe-statics")
 
-string(REGEX REPLACE ";" " " cxxflags "${cxxflags}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${cxxflags}")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostartfiles -nostdlib -Wl,--defsym=STACK_SIZE=92160")
+string (REGEX REPLACE ";" " " cxxflags "${cxxflags}")
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${cxxflags}")
+set (CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostartfiles -nostdlib -Wl,--defsym=STACK_SIZE=92160")
 
-include(Sources.cmake)
+include (Sources.cmake)
 
 # The target chip that is compiled for:
-include(${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/sam3x8e.cmake)
+include (${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/sam3x8e.cmake)
 
 # The flashscript used to flash the binary (optional):
-include(${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/flashscripts/arduino_due.cmake)
+include (${toolchain}/targets/arm32/cm3/atmel/sam3x/sam3x8e/flashscripts/arduino_due.cmake)
--- modules/SHORTY/Sources.cmake
+++ modules/SHORTY/Sources.cmake
@@ -1,16 +1,16 @@
 # Libraries:
 
-link_libraries(gcc)
+link_libraries (gcc)
 
-set(hwlib ../../libraries/hwlib)
-include_directories(${hwlib}/library)
-add_definitions(-DBMPTK_TARGET_arduino_due
-                -DBMPTK_TARGET=arduino_due
-                -DBMPTK_BAUDRATE=19200)
+set (hwlib ../../libraries/hwlib)
+include_directories (${hwlib}/library)
+add_definitions (-DBMPTK_TARGET_arduino_due
+                 -DBMPTK_TARGET=arduino_due
+                 -DBMPTK_BAUDRATE=19200)
 
 # Source Files:
 
-set(sources
+set (sources
     src/wrap-hwlib.cpp
     src/libc-stub.cpp
     src/main.cpp
