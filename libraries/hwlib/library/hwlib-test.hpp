b8725e3a3c8c890a03be256e8726518961d576cf
Julian van Doorn <gigajulian@gmail.com>
2018-05-25 13:52:07 +0200

    Fixed hwlib::test linker issues

--- libraries/hwlib/library/hwlib-test.hpp
+++ libraries/hwlib/library/hwlib-test.hpp
@@ -22,10 +22,66 @@
 #include HWLIB_INCLUDE ( stdint.h )
 #include HWLIB_INCLUDE ( stdio.h )
 #include HWLIB_INCLUDE ( initializer_list )
+#include HWLIB_INCLUDE ( unistd.h )
+
+#include HWLIB_INCLUDE ( chrono )
+#include HWLIB_INCLUDE ( thread )
 
 namespace hwlib {
 
 namespace test {
+
+void wait_ns( int_fast32_t n );
+void wait_us( int_fast32_t n );
+void wait_ms( int_fast32_t n ); 
+bool uart_char_available();
+char uart_getc();
+void uart_putc( char c );
+
+#ifdef HWLIB_ONCE
+auto begin = std::chrono::high_resolution_clock::now();
+
+uint64_t now_ticks(){
+   auto end = std::chrono::high_resolution_clock::now();
+   return std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+}   
+
+uint64_t ticks_per_us(){
+   return 1;
+}   
+
+uint64_t now_us(){
+   return now_ticks() / ticks_per_us();
+}   
+
+void wait_us_busy( int_fast32_t n ){
+   std::this_thread::sleep_for(std::chrono::nanoseconds(n));
+}
+
+void wait_ns( int_fast32_t n ){ 
+   wait_us_busy( 1 + n / 1'000 );
+}
+
+void wait_us( int_fast32_t n ){ 
+   wait_us_busy( n );
+}
+
+void wait_ms( int_fast32_t n ){
+   wait_us( 1'000 * n );
+}  
+
+void uart_putc( char c ){
+    std::putc(c, stdout);
+}
+
+char uart_getc(){
+   return std::getc(stdin);   
+}
+
+bool HWLIB_WEAK uart_char_available(){
+   return 1;
+}
+#endif // HWLIB_ONCE
 
 /// pin_in implementation for unit testing
 template <int L = 256>
@@ -145,8 +201,7 @@ class pin_oc : public hwlib::pin_oc {
 private:
    test::pin_in<L_R> test_pin_in;
    test::pin_out<L_W> test_pin_out;
-   uint8_t direction = -1; // undefined pin direction
-   
+
 public:
 
    /// \brief
@@ -160,29 +215,17 @@ public:
    pin_oc(Args... lst) : test_pin_in { lst... }
    {}
    
-   void direction_set_input() override {
-      direction = 0;
-   }
-   
    bool get(
       hwlib::buffering buf = hwlib::buffering::unbuffered 
    ) override {
-      if (direction == 0) {
-         return test_pin_in.get();
-      }
+       return test_pin_in.get();
    }   
-   
-   void direction_set_output() override {
-      direction = 1;
-   }
    
    void set( 
       bool v,
       hwlib::buffering buf = hwlib::buffering::unbuffered 
    ) override {
-      if (direction == 1) {
-         test_pin_out.set(v);
-      }
+      test_pin_out.set(v);
    }
 
    /// \brief
@@ -220,58 +263,53 @@ public:
 
 }; // namespace test
 
-#ifdef HWLIB_ONCE 
+void HWLIB_WEAK uart_putc( char c ){
+   test::uart_putc( c );
+}
+
+bool HWLIB_WEAK uart_char_available(){
+   return test::uart_char_available();
+}
+
+char HWLIB_WEAK uart_getc( ){
+   return test::uart_getc();
+}
+
+#ifdef HWLIB_ONCE
 
 uint64_t now_ticks(){
-   time_t msec = time(NULL) * 1000;
-   return msec;
+   return test::now_ticks();
 }   
 
 uint64_t ticks_per_us(){
-   return 1;
+   return test::ticks_per_us();
 }   
 
 uint64_t now_us(){
    return now_ticks() / ticks_per_us();
 }   
 
+void wait_ns( int_fast32_t n ){ 
+   test::wait_ns( n );
+}
+
 void wait_us_busy( int_fast32_t n ){
    auto end = now_us() + n;
    while( now_us() < end ){}
 }
 
-void wait_ns( int_fast32_t n ){ 
-   wait_us_busy( 1 + n / 1'000 );
-}
-
-void wait_us( int_fast32_t n ){ 
+void HWLIB_WEAK wait_us( int_fast32_t n ){ 
    wait_us_busy( n );
 }
 
-void wait_ms( int_fast32_t n ){
-   wait_us( 1'000 * n );
-// why doesn't this work (asny more)??
-/*
+void HWLIB_WEAK wait_ms( int_fast32_t n ){
    while( n > 0 ){
       wait_us( 1'000 );
       --n;
    }
-*/
 }
 
-void uart_putc( char c ) {
-    putc(c, stdout);
-}
-
-char uart_getc(){
-   return getc(stdin);   
-}
-
-bool HWLIB_WEAK uart_char_available() {
-   return 1;
-}
-
-#endif // #ifdef HBLIB_ONCE
+#endif
 
 }; // namespace hwlib
 
