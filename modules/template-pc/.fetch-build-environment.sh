a339bf5063a1f12aca95e9634af06de03c798180
Julian van Doorn <gigajulian@gmail.com>
2018-06-01 11:14:11 +0200

    Updated .fetch-build-environment.sh

--- modules/template-pc/.fetch-build-environment.sh
+++ modules/template-pc/.fetch-build-environment.sh
@@ -1,9 +1,9 @@
-# This file clones and installs the specified build environment
+# This file clones the specified
 
 github_URL="https://github.com/R2D2-2018/Build-environment"
 
 if [ -z ${R2D2_BUILD_ENVIRONMENT} ]; then
-git clone ${github_URL} $1
+git clone ${github_URL} $1 --recursive
 build_environment="$1"
 else
 build_environment=${R2D2_BUILD_ENVIRONMENT}
